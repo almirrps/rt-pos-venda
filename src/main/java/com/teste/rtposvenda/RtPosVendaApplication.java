b86a7ea5fa4a349dd6846b8bc08b23e320545185
almirrps <almirrps@hotmail.com>
2024-04-29 17:52:25 -0300

    Criando projeto rt-pos-venda

---
+++ src/main/java/com/teste/rtposvenda/RtPosVendaApplication.java
@@ -0,0 +1,13 @@
+package com.teste.rtposvenda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RtPosVendaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RtPosVendaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/teste/rtposvenda/RtPosVendaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teste.rtposvenda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RtPosVendaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
