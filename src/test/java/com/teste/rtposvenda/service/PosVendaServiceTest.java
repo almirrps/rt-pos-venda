462b9b2d68a51d142546b739873f65cd28732183
almirrps <almirrps@hotmail.com>
2024-05-10 11:06:09 -0300

    Realizado o teste unitário Feign

--- src/test/java/com/teste/rtposvenda/service/PosVendaServiceTest.java
+++ src/test/java/com/teste/rtposvenda/service/PosVendaServiceTest.java
@@ -1,16 +1,13 @@
 package com.teste.rtposvenda.service;
 
-import static com.teste.rtposvenda.shared.MockFactory.NOVA_QUANTIDADE_PARCELAS;
 import static com.teste.rtposvenda.shared.MockFactory.NOVO_DIA_PAGAMENTO;
 import static org.mockito.MockitoAnnotations.openMocks;
-import static org.springframework.test.util.ReflectionTestUtils.setField;
 
 import com.teste.rtposvenda.configuration.exception.FalhaGenericaAplicacao;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
-import org.mockito.Mockito;
 
 
 public class PosVendaServiceTest {
